9adb8b0f430277d2c0ef3a48b583be6995cf1348
rritoch <rritoch@gmail.com>
2013-07-12 15:53:06 +0400

    Added Linux Startup Script

---
+++ root/boot/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Start PikeVM
+#
+
+# Change the path below to the location of your pike eecutable
+PIKEBIN="/usr/local/bin/pike"
+
+
+##
+## Do not edit below this point unless you know what you are doing
+##
+
+ORIGDIR=`pwd`
+BOOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $ORIGDIR
+
+PIKEMASTER="${BOOTDIR}/master-1.0.pike"
+PIKESYS="${BOOTDIR}/system-1.0"
+PIKELOGERR="${BOOTDIR}/error.log"
+
+$PIKEBIN -m $PIKEMASTER $PIKESYS
+
