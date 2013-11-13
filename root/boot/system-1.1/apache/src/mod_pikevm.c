25f6ca891d34cad1960589b320864ba343a71abe
rritoch <rritoch@gmail.com>
2013-11-14 00:59:43 +0800

    Build system repairs

--- root/boot/system-1.1/apache/src/mod_pikevm.c
+++ root/boot/system-1.1/apache/src/mod_pikevm.c
@@ -1,7 +1,7 @@
 /* mod_pikevm.c:  */
 #include <stdio.h>
 #include "apr_hash.h"
-//#include "ap_config.h"
+#include "ap_config.h"
 #include "ap_provider.h"
 #include "httpd.h"
 #include "http_core.h"
